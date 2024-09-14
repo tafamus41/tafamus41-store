3aabd1c6a96f88db05acbc1e1793ff9e193f2019
asimov <seyranmustafa4@gmail.com>
2024-09-15 02:05:51 +0300

    p

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,1 +1,18 @@
-//AuthContext
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+// export const {Provider} = createContext()
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  console.log(user);
+  const login = (info) => {
+    setUser(info);
+  };
+  return (
+    <AuthContext.Provider value={{ user, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+export default AuthProvider;
