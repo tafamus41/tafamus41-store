7f19fea9d6c9794e48b1373ad6ca05f7b3af64b8
asimov <seyranmustafa4@gmail.com>
2024-09-15 01:44:46 +0300

    p

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email.toLocaleLowerCase() === "admin@aa.com" && pass === "admin") {
+      // console.log({ email, pass });
+      login({ email, pass });
+    } else {
+      alert("Kullanıcı bilgileri yanlış");
+    }
+  };
+
   return (
     <div className="loginDiv">
       <div className="h-[500px] w-11/12 sm:w-[475px] bg-white rounded-[20px] p-5 flex flex-col justify-center text-center">
@@ -16,7 +28,7 @@ const Login = () => {
             Enter your credetentials to access your account
           </p>
         </div>
-        <form className="flex flex-col text-left p-3 gap-5">
+        <form className="flex flex-col text-left p-3 gap-5" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-2">
             <label
               className="font-montserrat text-label text-labelColor hover:after:content-['admin@aa.com'] hover:after:text-black hover:after:pl-3 hover:after:underline hover:cursor-pointer"
@@ -30,6 +42,7 @@ const Login = () => {
               id="email"
               placeholder="Enter your email"
               required
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="flex flex-col gap-2">
@@ -45,15 +58,21 @@ const Login = () => {
               id="password"
               placeholder="Enter your password"
               required
+              onChange={(e) => setPass(e.target.value)}
             />
           </div>
+          <button className="bg-main h-[44px] font-montserrat text-label text-white uppercase hover:opacity-90 rounded-[4px]">
+            Sign In
+          </button>
         </form>
-        <button className="bg-main h-[44px] font-montserrat text-label text-white uppercase hover:opacity-90 rounded-[4px]">
-          Sign In
-        </button>
-        <div className="flex justify-center items-center flex-wrap">
-          <span className="text-label font-montserrat font-[400] text-labelColor">Forgot your password</span>
-          <span className="font-montserrat font-[500] text-main ml-1 underline text-[14px]">Reset password</span>
+
+        <div className="flex justify-center items-center flex-wrap mt-2">
+          <span className="text-label font-montserrat font-[500] text-labelColor">
+            Forgot your password
+          </span>
+          <span className="font-montserrat font-[500] text-main ml-1 underline text-[14px]">
+            Reset password
+          </span>
         </div>
       </div>
     </div>
