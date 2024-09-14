96a8a875812e4fbe691c4d1dfe74a78a024d579a
asimov <seyranmustafa4@gmail.com>
2024-09-15 00:19:16 +0300

    p

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Login = () => {
   return (
     <div className="loginDiv">
-      <div></div>
+      <div className="h-[500px] w-11/12 sm:w-[475px] bg-white rounded-[20px] p-5 flex flex-col justify-center text-center">
+        <div>
+          <span></span>
+          <h1 className="text-[22px] sm:text-[32px]" >Tafamus41 Store</h1>
+        </div>
+      </div>
     </div>
   );
 };
