1c88685220759fa877471813602c964db37ffdc6
asimov <seyranmustafa4@gmail.com>
2024-09-17 12:03:25 +0300

    p

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
@@ -6,17 +5,26 @@ import Products from "../pages/Products";
 import About from "../pages/About";
 import NotFound from "../pages/NotFound";
 import Navbar from "../components/Navbar";
+import PrivateRouter from "./PrivateRouter";
+import ProductDetail from "../pages/ProductDetail";
 
 const AppRouter = () => {
   return (
+    //* AuthProvider içinde react-router-dom'a ait yapıları kullanabilmek için index.js'e taşıdık
     // <BrowserRouter>
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Home />} />
-        <Route path="/dashboard/products" element={<Products />} />
-        <Route path="/dashboard/about" element={<About />} />
+        <Route path="/dashboard" element={<PrivateRouter />}>
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/dashboard/products" element={<Products />} />
+          <Route path="/dashboard/products/:id" element={<ProductDetail />} />
+          {/* //* Absolute path */}
+          <Route path="/dashboard/about" element={<About />} />
+          {/* //* relative path */}
+          {/* <Route path="about" element={<About />} /> */}
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
